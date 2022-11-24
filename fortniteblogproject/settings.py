49ce72c12f775f134a30400f7612fc826b8686ad
tktktakatuska <blue.mountain.syota@iclouud.com>
2022-11-24 19:24:12 +0900

    mediapath修正

--- fortniteblogproject/settings.py
+++ fortniteblogproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
 STATIC_URL = 'static/'
 
-MEDIA_ROOT = [os.path.join(BASE_DIR , 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 MEDIA_URL  = 'media/'
 
 
